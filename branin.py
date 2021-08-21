a7de22d024c9bca80448f951996b5877e3687189
trentmc <5305452+trentmc@users.noreply.github.com>
2021-08-21 09:46:30 +0200

    Add 1d test example. WIP on 2d branin, need to properly plot model output

--- branin.py
+++ branin.py
@@ -1,16 +1,15 @@
-from mpl_toolkits.mplot3d import Axes3D  
-
 import matplotlib
 matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import numpy as np
+from matplotlib import cm, pyplot 
+from mpl_toolkits.mplot3d import Axes3D
+import numpy
+from sklearn import gaussian_process
 
 def branin_mesh(X0, X1):
     #b,c,t = 5.1/(4.*(pi)**2), 5./pi, 1./(8.*pi)
     b,c,t = 0.12918450914398066, 1.5915494309189535, 0.039788735772973836
     u = X1 - b*X0**2 + c*X0 - 6
-    r = 10.*(1. - t) * np.cos(X0) + 10
+    r = 10.*(1. - t) * numpy.cos(X0) + 10
     Z = u**2 + r
     return Z
 
@@ -58,25 +57,50 @@ def arff_string(title, name, X, y):
         
     return s
 
-def main():
+def model_sin_1d():
+    # not branin, but useful for testing plotting and gpr
+    
+    # create data
+    npoints = 20
+    xvec = numpy.linspace(0.0, 10.0, npoints)
+    X = numpy.reshape(xvec, (npoints,1))
+    y = numpy.sin(xvec)
+
+    # build model
+    kernel = gaussian_process.kernels.RBF()
+    model = gaussian_process.GaussianProcessRegressor(
+        kernel=kernel, normalize_y=True, random_state=0)
+    model.fit(X, y)
+    print(f"score={model.score(X,y)}")
+    yhat = model.predict(X, return_std=False)
+    
+    # plot
+    fig, ax = pyplot.subplots()
+    line1, = ax.plot(xvec, y, label='y')
+    line2, = ax.plot(xvec, yhat, label='yhat', linestyle="dashed", marker="o")
+    ax.legend()
+    pyplot.show()
+
+def model_branin_2d():
     # Make data
     npoints = 15
-    X0_vec = np.linspace(-5., 10., npoints)
-    X1_vec = np.linspace(0., 15., npoints)
-    X0, X1 = np.meshgrid(X0_vec, X1_vec)
+    X0_vec = numpy.linspace(-5., 10., npoints)
+    X1_vec = numpy.linspace(0., 15., npoints)
+    X0, X1 = numpy.meshgrid(X0_vec, X1_vec)
     Z = branin_mesh(X0, X1)
 
-    # Plot the surface
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    surf = ax.plot_surface(X0, X1, Z, cmap=cm.coolwarm,
-                           linewidth=0, antialiased=False)
-    #plt.show() #HACK
+    # Shape data for modeling
+    X = numpy.empty((X0.shape[0]*X0.shape[1],2))
+    X[:,0] = numpy.ravel(X0)
+    X[:,1] = numpy.ravel(X1)
+    y = numpy.ravel(Z)
 
-    # Convert to 2d X and 1d y
-    X = np.empty((X0.shape[0]*X0.shape[1],2))
-    X[:,0] = np.ravel(X0)
-    X[:,1] = np.ravel(X1)
-    y = np.ravel(Z)
+    # Surface plot
+    fig, ax = pyplot.subplots(subplot_kw={"projection": "3d"})
+    ax.plot_surface(X0, X1, Z, cmap=cm.coolwarm,
+                    linewidth=0, antialiased=False)
+    pyplot.title("Data")
+    pyplot.show()
 
     # Output arff file
     s = arff_string("Branin Function", "branin", X, y)
@@ -84,17 +108,23 @@ def main():
     f.write(s)
     f.close()
 
-    # Build Gaussian Process model, plot its surface
-    import pdb; pdb.set_trace()
-    from sklearn.gaussian_process import GaussianProcessRegressor
-    from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
-    kernel = DotProduct() + WhiteKernel()
-    gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
-    gpr.fit(X, y)
-    s = gpr.score(X,y)
-    yhat = gpr.predict(X, return_std=False)
-    import pdb; pdb.set_trace()
-    
+    # Build model
+
+    kernel = gaussian_process.kernels.RBF()
+    model = gaussian_process.GaussianProcessRegressor(
+        kernel=kernel, normalize_y=True, random_state=0)
+    model.fit(X, y)
+    print(f"score={model.score(X,y)}")
+    yhat = model.predict(X, return_std=False)
+
+    # Plot model
+    fig, ax = pyplot.subplots(subplot_kw={"projection": "3d"})
+    ax.plot_surface(X0, X1, Z, cmap=cm.coolwarm,
+                    linewidth=0, antialiased=False)
+    pyplot.title("Model")
+    pyplot.show()
+
 
 if __name__ == "__main__":
-    main()
+    model_branin_2d()
+    #model_sin_1d()
