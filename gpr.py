6f28026d68efcb020237f4246eb747887782bd52
Calina Cenan <calina@cenan.net>
2021-09-17 19:40:27 +0000

    Remove asset printing.

--- gpr.py
+++ gpr.py
@@ -71,8 +71,6 @@ def run_gpr(local=False):
 
     with open(filename) as datafile:
         datafile.seek(0)
-        print(datafile.read())
-        datafile.seek(0)
         res = arff.load(datafile)
 
     print("Stacking data.")
